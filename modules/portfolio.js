da17b94b127ec99744efaf185fe06bfaf6a197cd
rgraue <rgraue@outlook.com>
2022-05-31 15:43:56 -0700

    fix

--- modules/portfolio.js
+++ modules/portfolio.js
@@ -127,9 +127,7 @@ class Portfolio {
                 let data = this.#calcHistory(range, res);   // data for chart (balance vs time)
                 let labels = [];
                 for (let i in range){
-                    let a = range[i].split('-');
-                    let n = Number(a[0]) + 1;
-                    labels.push(n + "-" + a[1]);
+                    labels.push(i);
                 }
 
                 let r = {
